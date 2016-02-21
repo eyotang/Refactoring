3e0fc0e5444928483e5bf8b6e94fbeba32b7e754
Ben Tang <ben.tang@ericsson.com>
2016-02-21 08:46:59 +0800

    Init the repository

---
+++ Template.py
@@ -0,0 +1,37 @@
+#!/usr/bin/env python
+'''
+Created on 2016/02/10
+
+@author: eyotang
+'''
+import re, sys, os, traceback, signal
+
+from abc import ABCMeta, abstractmethod
+
+
+
+
+
+
+
+
+def onsignal_int(signum, frame) :
+    print ("\nReceive SIGINT[Ctrl+C] to stop process by force !")
+    sys.exit(-1)
+
+def register_signal() :
+    signal.signal(signal.SIGINT, onsignal_int)
+
+def main() :
+    register_signal()
+
+    return 0
+
+
+
+if __name__ == '__main__' :
+    try :
+        sys.exit(main())
+    except Exception as e :
+        traceback.print_exc(file = sys.stderr)
+        sys.exit(2)
